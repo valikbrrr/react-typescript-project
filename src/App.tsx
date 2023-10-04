cb2a64de4bd927dc484ae5c22253c9207e12c31a
valik <valik.0404@mail.ru>
2023-10-04 17:51:30 +0300

    proj-add-tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import React from "react";
+const e = React.createElement;
+
 function App() {
-  return <h1>hello world</h1>;
+  // return <h1>hello</h1>;
+  return e("h1", {}, [
+    e(
+      "p",
+      { className: "parag" },
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem nostrum culpa consequuntur dignissimos eveniet sint rerum illo, magni quos optio quibusdam soluta earum quia architecto recusandae iusto neque incidunt praesentium"
+    ),
+    e("button", { className: "Button" }, "click me"),
+  ]);
 }
 
 export default App;
