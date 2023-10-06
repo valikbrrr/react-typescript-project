24e55b843021f9179dc95c87414e739729896756
valik <valik.0404@mail.ru>
2023-10-06 14:11:37 +0300

    commit-example-one

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from "react";
-const e = React.createElement;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React, { createElement as e, useState } from "react";
+// const e = React.createElement;
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return e("div", { className: "container" }, [
-    e("h1", { className: "bg-black", key: 1 }, "aaaaaaa"),
-    e("button", { className: "py-2 px-4 border", key: 2 }, "click me"),
+    e("h1", { className: "bg-black", key: 1 }, `aaaaaaa${count}`),
+    e(
+      "button",
+      {
+        className: "py-50 px-4 border m-0-auto",
+        key: 2,
+        onClick: () => setCount(count + 1),
+      },
+      "click me"
+    ),
   ]);
 }
 
