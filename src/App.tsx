a7f3749120e6384963ebbe88394452fc2608fcad
valik <valik.0404@mail.ru>
2023-10-09 12:57:01 +0300

    feat: add Hero and Header components

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React, { createElement as e, useState } from "react";
-// const e = React.createElement;
+import styles from "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return e("div", { className: "container" }, [
-    e("h1", { className: "bg-black", key: 1 }, `aaaaaaa${count}`),
-    e(
-      "button",
-      {
-        className: "py-50 px-4 border m-0-auto",
-        key: 2,
-        onClick: () => setCount(count + 1),
-      },
-      "click me"
-    ),
-  ]);
+  return (
+    <div className="container">
+      <div className="header max-w-100% h-100px mx-auto text-red-700">
+        aaaaaaaaaaa
+      </div>
+    </div>
+  );
 }
-
 export default App;
