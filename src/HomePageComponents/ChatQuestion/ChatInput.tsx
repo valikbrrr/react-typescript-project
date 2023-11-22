fc7e6b649209591a8e7504ad93e2ed5fe61666ba
valik <valik.0404@mail.ru>
2023-11-22 23:52:12 +0300

    feat: first ref (experemental)

--- src/HomePageComponents/ChatQuestion/ChatInput.tsx
+++ src/HomePageComponents/ChatQuestion/ChatInput.tsx
@@ -1,16 +1,33 @@
 import { Paperclip } from "lucide-react";
+import { Smile } from "lucide-react";
+import { useRef, useState } from "react";
 
 const ChatInput = () => {
+  const [str, setStr] = useState<string | null>(null);
+  const divRef = useRef<HTMLDivElement | null>(null);
+  console.log(str, "render");
+  console.log(divRef.current);
+
   return (
     <div className="flex flex-row border-t-2 border-red-800 h-28">
       <div className="text-slate-400 w-12 px-3 py-3 rounded-full hover:bg-gray-200 cursor-pointer scale-90">
         <Paperclip />
       </div>
       <input
-        className="border-l-2 border-slate-300"
+        className="outline-none"
         type="text"
-        aria-label="aaaaaaaaa"
+        placeholder="Введите сообщение"
+        onChange={(e) => setStr(e.target.value)}
       />
+      <div
+        ref={divRef}
+        className="text-slate-400 w-12 px-3 py-3 rounded-full hover:bg-gray-200 cursor-pointer scale-90"
+      >
+        <Smile />
+      </div>
+      {/* <div className="w-12">
+        <Send />
+      </div> */}
     </div>
   );
 };
